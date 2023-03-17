93f10a1855caaa572757ffba0dd3877d3bd1a119
s22277 <s22277@pjwstk.edu.pl>
2023-03-17 08:37:22 +0100

    Update main.py

--- PPY01/main.py
+++ PPY01/main.py
@@ -11,16 +11,14 @@
 import webbrowser
 import requests
 
-pageurl1 = input('Podaj pierwszy URL strony internetowej: ')
-date1 = input("Podaj date: ")
-pageurl2 = input('Podaj drugi URL strony internetowej: ')
-date2 = input("Podaj date: ")
-pageurl3 = input('Podaj trzeci URL strony internetowej: ')
-date3 = input("Podaj date: ")
+pageurl = input('Podaj URL strony internetowej: ')
+date1 = input("Podaj pierwsza date (rrrrmmdd): ")
+date2 = input("Podaj pierwsza druga (rrrrmmdd): ")
+date3 = input("Podaj pierwsza trzecia (rrrrmmdd): ")
 
-url1 ="http://archive.org/wayback/available?url="+pageurl1+"&timestamp="+str(date1)
-url2 ="http://archive.org/wayback/available?url="+pageurl2+"&timestamp="+str(date2)
-url3 ="http://archive.org/wayback/available?url="+pageurl3+"&timestamp="+str(date3)
+url1 ="http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date1)
+url2 ="http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date2)
+url3 ="http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date3)
 
 response = requests.get(url1)
 d = response.json()
