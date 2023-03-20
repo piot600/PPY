5c87893a6960b77ebb27293862e943df1db0cf51
s22277 <s22277@pjwstk.edu.pl>
2023-03-20 14:59:15 +0100

    Update main.py

--- PPY02/main.py
+++ PPY02/main.py
@@ -1,1 +1,42 @@
-print("hello")
+
+
+# print("ANKIETA")
+#
+# pytanie1 = input("1. Jak masz na imie oraz nazwiko? \n")
+# pytanie2 = input("2. Po książki w jakiej formie sięgasz najczęściej? \n "
+#                  "\ta) papierowej (tradycyjnej)\n"
+#                  "\tb) e-booki (książki elektroniczne) na komputerze\n"
+#                  "\tc) e-booki na tablecie/telefonie\n")
+# if pytanie2 == "a":
+#     pytanie2 = "papierowej (tradycyjnej)"
+# elif pytanie2 == "b":
+#     pytanie2 = "e-booki (książki elektroniczne) na komputerze"
+# elif pytanie2 == "c":
+#     pytanie2 = "e-booki na tablecie/telefonie"
+# else:
+#     pytanie2 = "brak odpowiedzi"
+#
+# pytanie3 = input("3. Ile książek czytasz średnio w ciągu roku? \n"
+#                  "\ta. 0\n"
+#                  "\tb. 1-4\n"
+#                  "\tc. powyżej 4\n")
+# if pytanie3 == "a":
+#     pytanie3 = "0"
+# elif pytanie3 == "b":
+#     pytanie3 = "1-4"
+# elif pytanie3 == "c":
+#     pytanie3 = "powyżej 4"
+# else:
+#     pytanie3 = "brak odpowiedzi"
+#
+# pytanie4 = input("4. W jakim języku czytasz książki? \n"
+#                  "\t polskim\n"
+#                  "\t angielskim\n"
+#                  "\t hiszpańskim \n"
+#                  "\t innym (jakim?)\n")
+#
+# print("\nODPOWIEDZI:")
+# print("pytanie: Jak masz na imie oraz nazwiko? \n odpowiedź: ", pytanie1)
+# print("pytanie: Po książki w jakiej formie sięgasz najczęściej? \n odpowiedź: ", pytanie2)
+# print("pytanie: Ile książek czytasz średnio w ciągu roku? \n odpowiedź: ", pytanie3)
+# print("pytanie: W jakim języku czytasz książki?  \n odpowiedź: ", pytanie4)
